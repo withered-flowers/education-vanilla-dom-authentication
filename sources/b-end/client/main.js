f43077b9977cf85c2b4d6a86c8a29cd2fe71f9d0
Withered Flowers <withered-flowers@users.noreply.github.com>
2023-10-26 10:20:12 +0700

    feat: delete db and start b-end

---
+++ sources/b-end/client/main.js
@@ -0,0 +1,54 @@
+import axios from "axios";
+import "./style.css";
+
+// Function Declaration
+const doAuthentication = (e) => {
+  e.preventDefault();
+
+  const username = document.querySelector("#formLoginEmail").value;
+  const password = document.querySelector("#formLoginPassword").value;
+
+  console.log(username, password);
+};
+
+const fetchTodosAndRenderTable = async () => {
+  try {
+    const { data } = await axios.get(
+      "http://jsonplaceholder.typicode.com/todos"
+    );
+
+    // Only get First 10 data
+    const todos = data.slice(0, 10);
+
+    // Render todos to DOM (table - privateTableBody)
+    const privateTableBody = document.querySelector("#privateTableBody");
+
+    const innerTable = todos.map((todo) => {
+      return `
+        <tr>
+          <td>${todo.id}</td>
+          <td>${todo.title}</td>
+          <td>${todo.completed}</td>
+        </tr>
+      `;
+    });
+
+    privateTableBody.innerHTML = innerTable.join("");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const initialize = () => {
+  // Hide #privateRoute
+  document.querySelector("#privateRoute").classList.toggle("hidden");
+
+  // Add event submit (fn doAuthentication) to #formLogin
+  document
+    .querySelector("#formLogin")
+    .addEventListener("submit", doAuthentication);
+};
+// End of Function Declaration
+
+// Runner
+initialize();
